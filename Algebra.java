56042786b441d4082a393c1faaa71615134c23f5
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-26 19:59:48 +0200

    test algabra

--- Algebra.java
+++ Algebra.java
@@ -6,12 +6,12 @@
 public class Algebra {
 	public static void main(String args[]) {
 		// Tests some of the operations
-		// System.out.println(plus(2, 3)); // 2 + 3
+		// System.out.println(plus(6, -5)); // 2 + 3
 		// System.out.println(minus(7, 2)); // 7 - 2
 		// System.out.println(minus(2, 7)); // 2 - 7
 		// System.out.println(times(3, 4)); // 3 * 4
 		// System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
-		// System.out.println(pow(5, 3)); // 5^3
+		// System.out.println(pow(2, 3)); // 5^3
 		// System.out.println(pow(3, 5)); // 3^5
 		System.out.println(div(12, 3)); // 12 / 3
 		// System.out.println(div(5, 5)); // 5 / 5
@@ -23,65 +23,107 @@ public class Algebra {
 		// System.out.println(sqrt(76123));
 	}
 
-	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
-		for (int i = 0; i < x2; i++) {
-			x1++;
+
+		if (x2 < 0) {
+			int Z = Math.abs(x2);
+			for (int i = 0; i < Z; i++) {
+				x1--;
+			}
+			return x1;
 		}
-		return x1;
+
+		else {
+			for (int i = 0; i < x2; i++) {
+				x1++;
+			}
+			return x1;
+		}
 
 	}
 
-	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// Replace the following statement with your code
-		for (int i = 0; i < x2; i++) {
-			x1--;
+		if (x2 > 0) {
+			for (int i = 0; i < x2; i++) {
+				x1--;
+			}
+			return x1;
 		}
-		return 0;
+
+		else {
+			int z = Math.abs(x2);
+			for (int i = 0; i < z; i++) {
+				x1++;
+			}
+			return x1;
+		}
 	}
 
-	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		// Replace the following statement with your code
 		int times = 0;
-		for (int i = 0; i < x2; i++) {
+		while (x1 != 0 && x2 != 0) {
 
-			for (int j = 0; j < x1; j++) {
-				times++;
+			if (x1 > 0 && x2 < 0) {
+				for (int i = 0; i < x1; i++) {
+					times += x2;
+				}
+				return times;
+			}
+			if (x1 < 0 && x2 > 0) {
+				for (int i = 0; i < x2; i++) {
+					times += x1;
+				}
+				return times;
 			}
 
+			else {
+				for (int i = 0; i < x2; i++) {
+					for (int j = 0; j < x1; j++) {
+						times++;
+					}
+
+				}
+				return times;
+			}
 		}
 		return times;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		int pow = 0;
+		int pow = 1;
 		for (int i = 0; i < n; i++) {
-			pow = times(times(x, x), x);
+			pow = times(pow, x);
 		}
 		return pow;
 	}
 
 	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
+		int counter = 0;
 		while (x2 != 0) {
 			if (x1 == 0) {
 				return 0;
 			}
+
 			if (x1 == x2) {
-				return 1;
+				return counter += 1;
 			}
 			if (x1 > x2) {
-				return minus(x1, x2);
+				while (x1 != 0) {
+					x1 = x1 - x2;
+					counter++;
+				}
+				return counter;
 			}
+			if (x2 > x1) {
 
+			}
 		}
-		return 0;
+		return counter;
 	}
 
 	// Returns x1 % x2
