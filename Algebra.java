804978fe9fd3e9d92d9eb02fbb09454785450934
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-26 21:19:03 +0200

    test algabra 3

--- Algebra.java
+++ Algebra.java
@@ -11,10 +11,10 @@ public class Algebra {
 		// System.out.println(minus(2, 7)); // 2 - 7
 		// System.out.println(times(-3, -4)); // 3 * 4
 		// System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
-		System.out.println(pow(-2, 2)); // 5^3
+		// System.out.println(pow(-2, 2)); // 5^3
 		// System.out.println(pow(3, 5)); // 3^5
 		// System.out.println(div(12, 3)); // 12 / 3
-		// System.out.println(div(5, 5)); // 5 / 5
+		System.out.println(div(12, 3)); // 5 / 5
 		// System.out.println(div(25, 7)); // 25 / 7
 		// System.out.println(mod(25, 7)); // 25 % 7
 		// System.out.println(mod(120, 6)); // 120 % 6
@@ -113,7 +113,10 @@ public class Algebra {
 	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
 		int counter = 0;
+		int mark;
+		mark = (x1 < 0 || x2 < 0) ? -1 : 1;
 		while (x2 != 0) {
+
 			if (x1 == 0) {
 				return 0;
 			}
@@ -121,17 +124,24 @@ public class Algebra {
 			if (x1 == x2) {
 				return counter += 1;
 			}
+
 			if (x1 > x2) {
-				while (x1 != 0) {
-					x1 = x1 - x2;
+				for (int i = 0; x1 == 0; i++) {
+					x1 = minus(x1, x2);
 					counter++;
 				}
-				return counter;
+				return counter = times(counter, mark);
 			}
-			if (x2 > x1) {
 
+			if (x2 > x1) {
+				for (int i = 0; x2 == 0; i++) {
+					x2 = minus(x2, x1);
+					counter++;
+				}
+				return counter = times(counter, mark);
 			}
 		}
+
 		return counter;
 	}
 
