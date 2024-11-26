2e6e7f44328ab856fb56bee5e482ede09586fc28
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-26 20:24:20 +0200

    test algabra2

--- Algebra.java
+++ Algebra.java
@@ -9,11 +9,11 @@ public class Algebra {
 		// System.out.println(plus(6, -5)); // 2 + 3
 		// System.out.println(minus(7, 2)); // 7 - 2
 		// System.out.println(minus(2, 7)); // 2 - 7
-		// System.out.println(times(3, 4)); // 3 * 4
+		// System.out.println(times(-3, -4)); // 3 * 4
 		// System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
-		// System.out.println(pow(2, 3)); // 5^3
+		System.out.println(pow(-2, 2)); // 5^3
 		// System.out.println(pow(3, 5)); // 3^5
-		System.out.println(div(12, 3)); // 12 / 3
+		// System.out.println(div(12, 3)); // 12 / 3
 		// System.out.println(div(5, 5)); // 5 / 5
 		// System.out.println(div(25, 7)); // 25 / 7
 		// System.out.println(mod(25, 7)); // 25 % 7
@@ -78,8 +78,17 @@ public class Algebra {
 				}
 				return times;
 			}
+			if (x1 < 0 && x2 < 0) {
+				x1 = -(x1);
+				x2 = -(x2);
+				for (int i = 0; i < x2; i++) {
+					for (int j = 0; j < x1; j++) {
+						times++;
+					}
 
-			else {
+				}
+				return times;
+			} else {
 				for (int i = 0; i < x2; i++) {
 					for (int j = 0; j < x1; j++) {
 						times++;
