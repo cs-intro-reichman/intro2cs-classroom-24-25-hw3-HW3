94aa2b7a17dcd82fdc3363c5bf52d4933807ee77
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-27 15:49:11 +0200

    loancalc1

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,54 +1,65 @@
 // Computes the periodical payment necessary to pay a given loan.
 public class LoanCalc {
-	
-	static double epsilon = 0.001;  // Approximation accuracy
-	static int iterationCounter;    // Number of iterations 
-	
+
+	static double epsilon = 0.001; // Approximation accuracy
+	static int iterationCounter; // Number of iterations
+
 	// Gets the loan data and computes the periodical payment.
-    // Expects to get three command-line arguments: loan amount (double),
-    // interest rate (double, as a percentage), and number of payments (int).  
-	public static void main(String[] args) {		
+	// Expects to get three command-line arguments: loan amount (double),
+	// interest rate (double, as a percentage), and number of payments (int).
+	public static void main(String[] args) {
 		// Gets the loan data
 		double loan = Double.parseDouble(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
+		double payment = Double.parseDouble(args[3]);
+		// System.out.println("Loan = " + loan + ", interest rate = " + rate +
+		// "%,periods = " + n);
 
-		// Computes the periodical payment using brute force search
+		// // Computes the periodical payment using brute force search
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
 
-		// Computes the periodical payment using bisection search
+		// // Computes the periodical payment using bisection search
 		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
 	}
 
-	// Computes the ending balance of a loan, given the loan amount, the periodical
-	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+	private static double endBalance(double loan, double rate, int n, double payment) {
+
+		for (int i = 0; i <= n; i++) {
+			loan = (loan - payment) * rate;
+		}
+		return loan;
 	}
-	
+
 	// Uses sequential search to compute an approximation of the periodical payment
 	// that will bring the ending balance of a loan close to 0.
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
+		int counterPayments = 0;
+		double payment = loan / n;
+		double calcForce = endBalance(loan, rate, n, payment);
+		while (calcForce > epsilon) {
+			payment += epsilon;
+			counterPayments++;
+			calcForce = endBalance(loan, rate, n, payment);
+		}
 		// Replace the following statement with your code
-		return 0;
-    }
-    
-    // Uses bisection search to compute an approximation of the periodical payment 
+		return payment;
+	}
+
+	// Uses bisection search to compute an approximation of the periodical payment
 	// that will bring the ending balance of a loan close to 0.
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
-    public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
+	public static double bisectionSolver(double loan, double rate, int n, double epsilon) {
+		// Replace the following statement with your code
 		return 0;
-    }
+	}
 }
