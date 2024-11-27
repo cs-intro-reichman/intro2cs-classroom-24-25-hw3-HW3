4097084b52a9778d680d668e169c26c2b9ff2157
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-28 00:32:11 +0200

    algabrafinal

--- Algebra.java
+++ Algebra.java
@@ -13,7 +13,7 @@ public class Algebra {
 		// System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
 		// System.out.println(pow(-2, 2)); // 5^3
 		// System.out.println(pow(3, 5)); // 3^5
-		System.out.println(div(9, 3)); // 12 / 3
+		// System.out.println(div(9, 3)); // 12 / 3
 		// System.out.println(div(12, 3)); // 5 / 5
 		// System.out.println(div(25, 7)); // 25 / 7
 		// System.out.println(mod(25, 7)); // 25 % 7
@@ -112,7 +112,7 @@ public class Algebra {
 
 	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
-		int counter = 1;
+		int counter = 0;
 		int mark;
 		mark = (x1 < 0 || x2 < 0) ? -1 : 1;
 		while (x2 != 0 && x1 >= x2) {
